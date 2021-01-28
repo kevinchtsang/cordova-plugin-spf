4496384a25d6a245cfa39ab3a354c050b6169802
Kevin Tsang <K.C.H.Tsang@sms.ed.ac.uk>
2021-01-28 17:52:43 +0000

    match function names to .h and .m

--- www/spf.js
+++ www/spf.js
@@ -1,16 +1,16 @@
 /*global cordova, module*/
 
 module.exports = {
-    startCalibration: function (successCallback, errorCallback) {
+    startCalibration:SRP function (successCallback, errorCallback) {
         cordova.exec(successCallback, errorCallback);
     },
-    stopCalibration: function (successCallback, errorCallback) {
+    stopCalibrationSRP: function (successCallback, errorCallback) {
         cordova.exec(successCallback, errorCallback);
     },
-    startMeasurement: function (successCallback, errorCallback) {
+    startMeasurementSRP: function (successCallback, errorCallback) {
         cordova.exec(successCallback, errorCallback);
     },
-    stopMeasurement: function (successCallback, errorCallback) {
+    stopMeasurementSRP: function (successCallback, errorCallback) {
         cordova.exec(successCallback, errorCallback);
     }
 };
