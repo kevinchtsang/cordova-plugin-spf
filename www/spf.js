2916e5a666dc54f64b77f84efb55b8386867ad8b
Kevin Tsang <K.C.H.Tsang@sms.ed.ac.uk>
2021-01-29 19:24:13 +0000

    correct js file

--- www/spf.js
+++ www/spf.js
@@ -1,16 +1,16 @@
 /*global cordova, module*/
 
 module.exports = {
-    startCalibration:SRP function (successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback);
+    startCalibrationSRP: function (successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "spf", "startCalibrationSRP");
     },
     stopCalibrationSRP: function (successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback);
+        cordova.exec(successCallback, errorCallback, "spf", "stopCalibrationSRP");
     },
     startMeasurementSRP: function (successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback);
+        cordova.exec(successCallback, errorCallback, "spf", "startMeasurementSRP");
     },
     stopMeasurementSRP: function (successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback);
+        cordova.exec(successCallback, errorCallback, "spf", "stopMeasurementSRP");
     }
 };
